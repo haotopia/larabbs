4520a2652dd651207b5be2eafd08e1da9ae677e0
haotopia <haotopia@icloud.com>
2018-04-27 09:32:52 +0000

    User can upload avatar

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\user;
 
@@ -12,8 +13,17 @@ class UsersController extends Controller {
 	public function edit(User $user) {
 		return view('users.edit', compact('user'));
 	}
-	public function update(UserRequest $request, User $user) {
-		$user->update($request->all());
+	public function update(UserRequest $request, User $user, ImageUploadHandler $uploader) {
+
+		$data = $request->all();
+		if ($request->avatar) {
+			$result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
+			if ($result) {
+				$data['avatar'] = $result['path'];
+			}
+		}
+
+		$user->update($data);
 		return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
 	}
 	public function messages() {
